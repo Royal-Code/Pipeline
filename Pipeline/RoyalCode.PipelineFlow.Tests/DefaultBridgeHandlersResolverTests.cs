9bb21c440af164b837a0ba6f1a14a0f9e40b1b06
Glauco Knihs <glauco.knihs@havan.com.br>
2021-08-26 19:48:28 -0300

    tests

---
+++ Pipeline/RoyalCode.PipelineFlow.Tests/DefaultBridgeHandlersResolverTests.cs
@@ -0,0 +1,34 @@
+﻿using RoyalCode.PipelineFlow.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoyalCode.PipelineFlow.Tests
+{
+    public class DefaultBridgeHandlersResolverTests
+    {
+        [Fact]
+        public void _01_Action_Handler()
+        {
+            string backValue = null;
+
+            var resolver = DefaultHandlersResolver.BridgeHandler<int, string>((i, next) => next(i.ToString()));
+
+            var description = resolver.TryResolve(typeof(int));
+            Assert.NotNull(description);
+
+            var @delegate = description.HandlerDelegateProvider(typeof(int), typeof(void));
+            Assert.NotNull(@delegate);
+
+            var action = @delegate as Action<int, Action<string>>;
+            Assert.NotNull(action);
+
+            action(1, v => backValue = v);
+
+            Assert.Equal("1", backValue);
+        }
+    }
+}
--- Pipeline/RoyalCode.PipelineFlow.Tests/DefaultDecoratorsResolverTests.cs
+++ Pipeline/RoyalCode.PipelineFlow.Tests/DefaultDecoratorsResolverTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RoyalCode.PipelineFlow.Tests
 {
-    class DefaultDecoratorsResolverTests
+    public class DefaultDecoratorsResolverTests
     {
     }
 }
