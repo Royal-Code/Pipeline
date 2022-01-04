19e0622c17e47f6d1b08982f32b373760d3109b7
Glauco Knihs <eglauko@hotmail.com>
2022-01-03 23:17:41 -0300

    tratamento de erro e de entrega.

---
+++ Pipeline/RoyalCode.EventDispatcher/IEventDispatchCompletedHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoyalCode.EventDispatcher;
+
+/// <summary>
+/// <para>
+///     Handlers for when the event dispatch is successfully completed.
+/// </para>
+/// <para>
+///     Events will only be handled in dispatches whose strategy is of separate scope.
+/// </para>
+/// <para>
+///     Handle all types of events.
+/// </para>
+/// </summary>
+public interface IEventDispatchCompletedHandler 
+{
+    /// <summary>
+    /// Handler the event after dispatch is successfully completed.
+    /// </summary>
+    /// <param name="eventType">The event type.</param>
+    /// <param name="eventObject">The event object, data.</param>
+    /// <param name="observer">The observer that throw the exception.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Task of assynchronous operation.</returns>
+    Task HandleDispatchCompletedAsync(
+        Type eventType, object eventObject,
+        object observer, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// <para>
+///     Handlers for when the event dispatch is successfully completed.
+/// </para>
+/// <para>
+///     Events will only be handled in dispatches whose strategy is of separate scope.
+/// </para>
+/// <para>
+///     Handle events of type <typeparamref name="TEvent"/>.
+/// </para>
+/// </summary>
+/// <typeparam name="TEvent">The event type to handle.</typeparam>
+public interface IEventDispatchCompletedHandler<in TEvent>
+{
+    /// <summary>
+    /// Handler the event after dispatch is successfully completed.
+    /// </summary>
+    /// <param name="eventObject">The event object, data.</param>
+    /// <param name="observer">The observer that throw the exception.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Task of assynchronous operation.</returns>
+    Task HandleDispatchCompletedAsync(
+        TEvent eventObject, object observer, CancellationToken cancellationToken = default);
+}
---
+++ Pipeline/RoyalCode.EventDispatcher/IEventDispatchErrorHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoyalCode.EventDispatcher;
+
+/// <summary>
+/// <para>
+///     Error handlers when dispatching events to observers.
+/// </para>
+/// <para>
+///     Errors will only be handled in dispatches whose strategy is of separate scope.
+/// </para>
+/// <para>
+///     Handle all types of events.
+/// </para>
+/// </summary>
+public interface IEventDispatchErrorHandler
+{
+    /// <summary>
+    /// Handler the event error.
+    /// </summary>
+    /// <param name="eventType">The event type.</param>
+    /// <param name="eventObject">The event object, data.</param>
+    /// <param name="observer">The observer that throw the exception.</param>
+    /// <param name="exception">The exception, error.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Task of assynchronous operation.</returns>
+    Task HandleErrorAsync(
+        Type eventType, object eventObject,
+        object observer, Exception exception,
+        CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// <para>
+///     Error handlers when dispatching events to observers.
+/// </para>
+/// <para>
+///     Errors will only be handled in dispatches whose strategy is of separate scope.
+/// </para>
+/// <para>
+///     Handle events of type <typeparamref name="TEvent"/>.
+/// </para>
+/// </summary>
+/// <typeparam name="TEvent">The event type to handle.</typeparam>
+public interface IEventDispatchErrorHandler<TEvent>
+    where TEvent : class
+{
+    /// <summary>
+    /// Handler the event error.
+    /// </summary>
+    /// <param name="eventObject">The event object, data.</param>
+    /// <param name="observer">The observer that throw the exception.</param>
+    /// <param name="exception">The exception, error.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Task of assynchronous operation.</returns>
+    Task HandleErrorAsync(
+        TEvent eventObject,
+        object observer, Exception exception,
+        CancellationToken cancellationToken = default);
+}
