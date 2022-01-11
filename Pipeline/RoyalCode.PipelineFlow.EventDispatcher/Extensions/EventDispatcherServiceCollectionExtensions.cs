181685859efa24cdc1c7e8031ba902becc0c2d01
Glauco Knihs <eglauko@hotmail.com>
2022-01-10 23:37:42 -0300

    minimo

---
+++ Pipeline/RoyalCode.PipelineFlow.EventDispatcher/Extensions/EventDispatcherServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extensions methods for <see cref="IServiceCollection"/>.
+/// </summary>
+public static class EventDispatcherServiceCollectionExtensions
+{
+}
+
---
+++ Pipeline/RoyalCode.PipelineFlow.EventDispatcher/Internal/EventDispatcher.cs
@@ -0,0 +1,33 @@
+﻿using RoyalCode.EventDispatcher;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoyalCode.PipelineFlow.EventDispatcher.Internal;
+
+internal class EventDispatcher : IEventDispatcher
+{
+    private readonly IPipelineFactory<IEventDispatcher> factory;
+
+    public void Dispatch(Type eventType, object eventObject, DispatchStrategy strategy)
+    {
+        var request = new EventDispatchRequest();
+        var pipeline = factory.Create<EventDispatchRequest>();
+        pipeline.Send(request);
+    }
+
+    public Task DispatchAsync(Type eventType, object eventObject, DispatchStrategy strategy, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+#if NET5_0_OR_GREATER
+public record EventDispatchRequest(){}
+
+#else
+public class EventDispatchRequest
+{
+
+}
+#endif
