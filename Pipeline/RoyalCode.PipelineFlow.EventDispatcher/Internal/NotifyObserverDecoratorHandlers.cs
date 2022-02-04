0543ba1eec50e30e80eeff790a52bc88b1d78b2a
Glauco Knihs <eglauko@hotmail.com>
2022-02-04 00:32:41 -0300

    goto sleep...

--- Pipeline/RoyalCode.PipelineFlow.EventDispatcher/Internal/NotifyObserverDecoratorHandlers.cs
+++ Pipeline/RoyalCode.PipelineFlow.EventDispatcher/Internal/NotifyObserverDecoratorHandlers.cs
@@ -1,6 +1,8 @@
 ﻿
 using RoyalCode.EventDispatcher;
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -51,9 +53,7 @@ internal static class NotifyObserverDecoratorHandlers
         await next();
     }
 
-    public static void BuildNotifyInCurrentScope<TService, TEvent>(MethodInfo method)
-        where TService : class
-        where TEvent : class
+    public static void BuildNotifyInCurrentScope(MethodInfo method)
     {
         // use ObserverMethodResolver, or receive by parameter
     }
@@ -82,13 +82,77 @@ internal static class NotifyObserverDecoratorHandlers
 
 public class ObserverMethodResolver
 {
+    private readonly MethodInfo method;
+    private readonly bool isAsync;
+    private readonly Type eventType;
+    private readonly bool hasToken;
+
     public ObserverMethodResolver(MethodInfo method)
     {
         var parameters = method.GetParameters();
         if (parameters.Length == 0)
-            throw new InvalidObserverMethodException("Invalid observer method"); // create a better message and resource.
+            throw new InvalidObserverMethodException(
+                "Invalid observer method, it must have one parameter at least."); // create a better message and resource.
+        if (parameters.Length > 2)
+            throw new InvalidObserverMethodException(
+                "Invalid observer method, it must have one or two parameter, where the second parameter must be a cancellation token.");
 
+        this.method = method;
 
+        isAsync = ResolveIsAsync();
+        eventType = parameters[0].ParameterType;
+
+        if (!eventType.IsClass)
+            throw new InvalidObserverMethodException("The event type must be a class");
+
+        if (parameters.Length == 2)
+        {
+            if (!isAsync)
+                throw new InvalidObserverMethodException("Invalid observer method, to have two parameters, the method must be async.");
+
+            if (parameters[1].ParameterType != typeof(CancellationToken))
+                throw new InvalidObserverMethodException("Invalid observer method, the second parameter must be a cancellation token.");
+
+            hasToken = true;
+        }
+    }
+
+    public Delegate BuildCallerFunction()
+    {
+        // Func<TService, TEvent, CancellationToken, Task>
+
+        var serviceVar = Expression.Parameter(method.DeclaringType, "service");
+        var eventVar = Expression.Parameter(eventType, "event");
+        var tokenVar = Expression.Parameter(typeof(CancellationToken), "token");
+
+        var arguments = new LinkedList<Expression>();
+        arguments.AddLast(eventVar);
+        if (hasToken)
+            arguments.AddLast(tokenVar);
+
+        var call = Expression.Call(serviceVar, method, arguments);
+
+        Expression body;
+        if (isAsync)
+            body = call;
+        else
+            body = Expression.Block(call, Expression.Constant(Task.CompletedTask));
+
+        //var funcType = typeof(Func<,,,>)
+        //    .MakeGenericType(method.DeclaringType, eventType, typeof(CancellationToken), typeof(Task));
+
+        var lambda = Expression.Lambda(body, serviceVar, eventVar, tokenVar);
+
+        return lambda.Compile();
+    }
+
+    private bool ResolveIsAsync()
+    {
+        return method.ReturnType == typeof(Task)
+            ? true
+            : method.ReturnType == typeof(void)
+                ? false
+                : throw new InvalidObserverMethodException("Invalid observer method, the return type must be void or Task.");
     }
 }
 
